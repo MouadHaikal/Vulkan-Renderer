6cecd28191385084c10181eb11f849b4e9122397
Mouad Haikal <Mouad.Haikal@um6p.ma>
2025-03-08 20:13:24 +0000

    Minor Changes

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -5,7 +5,6 @@
 #include <cstdint>
 #include <cstring>
 #include <stdexcept>
-#include <vulkan/vulkan_core.h>
 
 
 //==================================Main Functions==================================
